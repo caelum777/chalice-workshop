a2d907eba978b04a6dbec9a28a927ac624c890e5
andres <agonzalez@growthaccelerationpartners.com>
2020-07-16 21:57:51 -0600

    add dynamodb

--- chalicelib/db.py
+++ chalicelib/db.py
@@ -67,3 +67,60 @@ class InMemoryTodoDB(TodoDB):
             item['state'] = state
         if metadata is not None:
             item['metadata'] = metadata
+
+
+class DynamoDBTodo(TodoDB):
+    def __init__(self, table_resource):
+        self._table = table_resource
+
+    def list_all_items(self):
+        response = self._table.scan()
+        return response['Items']
+
+    def list_items(self, username=DEFAULT_USERNAME):
+        response = self._table.query(
+            KeyConditionExpression=Key('username').eq(username)
+        )
+        return response['Items']
+
+    def add_item(self, description, metadata=None, username=DEFAULT_USERNAME):
+        uid = str(uuid4())
+        self._table.put_item(
+            Item={
+                'username': username,
+                'uid': uid,
+                'description': description,
+                'state': 'unstarted',
+                'metadata': metadata if metadata is not None else {},
+            }
+        )
+        return uid
+
+    def get_item(self, uid, username=DEFAULT_USERNAME):
+        response = self._table.get_item(
+            Key={
+                'username': username,
+                'uid': uid,
+            },
+        )
+        return response['Item']
+
+    def delete_item(self, uid, username=DEFAULT_USERNAME):
+        self._table.delete_item(
+            Key={
+                'username': username,
+                'uid': uid,
+            }
+        )
+
+    def update_item(self, uid, description=None, state=None,
+                    metadata=None, username=DEFAULT_USERNAME):
+        # We could also use update_item() with an UpdateExpression.
+        item = self.get_item(uid, username)
+        if description is not None:
+            item['description'] = description
+        if state is not None:
+            item['state'] = state
+        if metadata is not None:
+            item['metadata'] = metadata
+        self._table.put_item(Item=item)
